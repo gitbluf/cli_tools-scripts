04b71630ffdb09e006b41c567622c3d3ea8b722f
Marko Petrovic <22802784+GoParrot@users.noreply.github.com>
2018-07-05 20:30:36 +0200

    added email notification

--- dockerexitclean.sh
+++ dockerexitclean.sh
@@ -3,9 +3,12 @@
 for i in $(docker ps -f "status=exited" -q)
   do
     if [ -n $i ]; then
-      echo "$i"
+      echo "$i" >> /path/to/lastdockercleanup.txt
+      echo "$i" $(date) >> /path/to/dockerCleanHistory.txt
       docker rm "$i"
+      echo "Subject: Docker cleanup" && cat /path/to/lastdockercleanup.txt | sendmail  "SDLC-SRE@davita.com"
+      rm -rf /path/to/lastdockercleanup.txt
     else
-        echo "No containers with exit status"
+        echo "No containers with exit status" $(date) >> /path/to/dockerCleanHistory.txt
     fi
  done
